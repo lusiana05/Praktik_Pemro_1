b1a62e28b5960f81c694652b0074f5f697bac00b
Lusiana Ilmanastri Oktiviasusanti <ilmanastri@gmail.com>
2021-09-15 20:40:36 +0700

    commit

---
+++ Pertemuan 3/CobaVar.java
@@ -0,0 +1,10 @@
+public class CobaVar {
+    public static void main(String[] args) {
+        String namaLengkap = "Lusiana";
+        int umur = 18;
+        double tinggiBadan = 160;
+        System.out.println("Nama Saya " + namaLengkap);
+        System.out.println("Umur " + umur);
+        System.out.println("Tinggi badan saya " + tinggiBadan);
+    }
+}
