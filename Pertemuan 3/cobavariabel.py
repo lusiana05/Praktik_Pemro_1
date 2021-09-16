67e2ebaf6b261f56aa73e2ec5ffb248228b9536d
Lusiana Ilmanastri Oktiviasusanti <ilmanastri@gmail.com>
2021-09-16 12:10:03 +0700

    tugas pertemuan 3

---
+++ Pertemuan 3/cobavariabel.py
@@ -0,0 +1,9 @@
+namaLengkap = "Lusiana Ilmanastri Oktiviasusanti"
+namaPanggilan = "Lusiana"
+umur = 18
+tinggiBadan = 160
+
+print("Nama lengkap saya adalah " + namaLengkap)
+print("Nama panggilan saya adalah " + namaPanggilan)
+print("Umur saya adalah " + umur)
+print("Tinggi badan saya adalah " + tinggiBadan)
